ec813fe62bea69a6510de82cb88f009fbc5fd941
lee <lisubin@we.com>
2018-03-12 13:06:43 +0800

    add

--- README.md
+++ README.md
@@ -13,3 +13,10 @@ march action
 # Tomcat Install
 
  wget http://mirror.cc.columbia.edu/pub/software/apache/tomcat/tomcat-9/v9.0.6/bin/apache-tomcat-9.0.6.tar.gz
+
+
+# Git Configure
+
+git config –global credential.helper store
+
+
--- sharding-expand-api/src/main/java/com/abin/lee/sharding/expand/api/ShardingExpandApplication.java
+++ sharding-expand-api/src/main/java/com/abin/lee/sharding/expand/api/ShardingExpandApplication.java
@@ -3,6 +3,8 @@ package com.abin.lee.sharding.expand.api;
 import com.abin.lee.sharding.expand.api.datasource.DynamicDataSourceRegister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 /**
@@ -12,7 +14,13 @@ import org.springframework.context.annotation.Import;
  */
 @SpringBootApplication
 @Import({DynamicDataSourceRegister.class})
-public class ShardingExpandApplication {
+public class ShardingExpandApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(ShardingExpandApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShardingExpandApplication.class, args);
